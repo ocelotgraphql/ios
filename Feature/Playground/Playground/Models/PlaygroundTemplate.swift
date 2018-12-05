0884ade425142210a4afab57ca5057f70e421f4a
Moritz Lang <hi@slashmo.codes>
2018-12-05 14:41:05 +0100

    Add GitHub Playground template

--- Feature/Playground/Playground/Models/PlaygroundTemplate.swift
+++ Feature/Playground/Playground/Models/PlaygroundTemplate.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 extension Playground {
 	/// A `Playground.Template` provides the basis of which new
 	/// `Playground`s are created.
@@ -8,7 +10,15 @@ extension Playground {
 		/// The `Playground`s contents.
 		public let contents: String
 
+		/// The default template with default settings and an empty new line as contents.
 		public static let `default` = Template()
+
+		/// A `Playground.Template` for GitHubs GraphQL API.
+		public static let gitHub: Template = {
+			let endpoint = URL(string: "https://api.github.com/graphql")!
+			let settings = Settings(endpoint: endpoint)
+			return Template(settings: settings)
+		}()
 
 		private init(settings: Settings = Settings(), contents: String = "\n") {
 			self.settings = settings
---
+++ Feature/Playground/PlaygroundTests/Models/PlaygroundTemplateTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Playground
+
+final class PlaygroundTemplateTests: XCTestCase {
+	func testTheGitHubTemplateUsesTheCorrectSettingsAndContents() {
+		let endpoint = URL(string: "https://api.github.com/graphql")!
+		let template = Playground.Template.gitHub
+
+		XCTAssertEqual(template.settings, Playground.Settings(endpoint: endpoint))
+		XCTAssertEqual(template.contents, "\n")
+	}
+}
